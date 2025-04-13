3e1f194e91ddf802d8c46d48dee09666e25219c1
hyperdev <195338336+HyperDev69@users.noreply.github.com>
2025-04-13 18:45:50 +0200

    Better like this

--- Packets/Messages/Client/LoginMessage.py
+++ Packets/Messages/Client/LoginMessage.py
@@ -14,12 +14,11 @@ class LoginMessage(ByteStream):
         self.device = device
         self.data = data
         self.player = player  
-        
-    # Login infos start
+    
     def decode(self):
         self.settings = json.load(open('Settings.json'))
         self.isInMaintenance = self.settings["isInMaintenance"]
-        isInMaintenance = self.isInMaintenance
+
         self.loginPayload = {}
         self.loginPayload["highID"] = self.readInt()
         self.loginPayload["lowID"] = self.readInt()
@@ -36,22 +35,19 @@ class LoginMessage(ByteStream):
         self.loginPayload["region"] = self.readString().split('-')[1]
         self.player.usedVersion = self.loginPayload["majorVersion"]
 
-    def process(self):
-        if self.player.usedVersion == 4:
-            if self.loginPayload["token"] == b'':
-                self.loginPayload["token"] = "token"
-            else:
-                self.player.token = self.loginPayload["token"]
-            
-            self.player.high_id = self.loginPayload["highID"]
-            self.player.low_id = self.loginPayload["lowID"]
-            self.player.token = self.loginPayload["token"]
-            self.player.region = self.loginPayload["region"]
-
-            # Sending messages
-            LoginOkMessage(self.device, self.player, self.loginPayload).Send()
-            OwnHomeDataMessage(self.device, self.player).Send()
-            ClanData(self.device, self.player).Send()
-            ClanStream(self.device, self.player).Send()
-        if self.player.isInMaintenance:
+        if self.isInMaintenance:
             LoginFailedMessage(self.device, self.player, self.loginPayload, "Server currently under maintenance, try later!", 10).Send()
+            return
+
+        # Process
+        self.loginPayload["token"] = "tokeh"
+        self.player.high_id = self.loginPayload["highID"]
+        self.player.low_id = self.loginPayload["lowID"]
+        self.player.token = self.loginPayload["token"]
+        self.player.region = self.loginPayload["region"]
+
+        # Send login messages
+        LoginOkMessage(self.device, self.player, self.loginPayload).Send()
+        OwnHomeDataMessage(self.device, self.player).Send()
+        ClanData(self.device, self.player).Send()
+        ClanStream(self.device, self.player).Send()
